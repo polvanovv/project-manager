9f12c3f29f40b2111ac8407514d1adaf34f17545
Polvanov Igor <igor.polvanov@sibers.com>
2020-05-25 16:24:43 +0600

    display email if specified

--- manager/src/Security/UserIdentity.php
+++ manager/src/Security/UserIdentity.php
@@ -140,7 +140,6 @@ class UserIdentity implements UserInterface, EquatableInterface
 
         return
             $this->id === $user->id &&
-            $this->username === $user->username &&
             $this->password === $user->password &&
             $this->role === $user->role &&
             $this->status === $user->status;
