bb2e2c5a57c4b6690ddaf62be4479d9a97f15b1a
polvanovv <igor.polvanov@sibers.com>
2020-06-11 10:21:11 +0600

    sidebar bnp menu added

---
+++ manager/src/Menu/SidebarMenu.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace App\Menu;
+
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class SidebarMenu
+{
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $auth;
+
+    public function __construct(FactoryInterface $factory, AuthorizationCheckerInterface $auth)
+    {
+        $this->factory = $factory;
+        $this->auth = $auth;
+    }
+
+    public function build(): ItemInterface
+    {
+        $menu = $this->factory->createItem('root')
+            ->setChildrenAttributes(['class' => 'c-sidebar-nav']);
+
+        $menu->addChild('Dashboard', ['route' => 'home'])
+            ->setExtra('icon', 'c-sidebar-nav-icon icon-speedometer')
+            ->setAttribute('class', 'c-sidebar-nav-item')
+            ->setLinkAttribute('class', 'c-sidebar-nav-link');
+
+        $menu->addChild('Work')->setAttribute('class', 'c-sidebar-nav-label');
+
+        if ($this->auth->isGranted('ROLE_WORK_MANAGE_MEMBERS')) {
+            $menu->addChild('Members', ['route' => 'work_members'])
+                ->setExtra('routes', [
+                    ['route' => 'work_members'],
+                    ['pattern' => '/^work_members_.*/']
+                ])
+                ->setExtra('icon', 'c-sidebar-nav-icon icon-people')
+                ->setAttribute('class', 'c-sidebar-nav-item')
+                ->setLinkAttribute('class', 'c-sidebar-nav-link');
+        }
+
+        $menu->addChild('Control')->setAttribute('class', 'c-sidebar-nav-label');
+
+        if ($this->auth->isGranted('ROLE_MANAGE_USERS')) {
+            $menu->addChild('Users', ['route' => 'users'])
+                ->setExtra('routes', [
+                    ['route' => 'users'],
+                    ['pattern' => '/^users_.*/']
+                ])
+                ->setExtra('icon', 'c-sidebar-nav-icon icon-people')
+                ->setAttribute('class', 'c-sidebar-nav-item')
+                ->setLinkAttribute('class', 'c-sidebar-nav-link');
+        }
+
+        $menu->addChild('Profile', ['route' => 'profile'])
+            ->setExtra('routes', [
+                ['route' => 'profile'],
+                ['pattern' => '/^profile_.*/']
+            ])
+            ->setExtra('icon', 'c-sidebar-nav-icon icon-people')
+            ->setAttribute('class', 'c-sidebar-nav-item')
+            ->setLinkAttribute('class', 'c-sidebar-nav-link');
+
+        return $menu;
+    }
+}
