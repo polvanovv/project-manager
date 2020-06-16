ed00a606051d576ee8b58027d45c991d59f48225
polvanovv <igor.polvanov@sibers.com>
2020-06-16 21:05:17 +0600

    projects controllers added

---
+++ manager/src/Controller/Work/Projects/Project/ProjectController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller\Work\Projects\Project;
+
+
+use App\Model\Work\Entity\Projects\Project\Project;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/work/projects/{id}", name="work_projects_project")
+ */
+class ProjectController extends AbstractController
+{
+    /**
+     * @Route("", name="_show")
+     * @param Project $project
+     * @return Response
+     */
+    public function show(Project $project): Response
+    {
+        return $this->render('app/work/projects/project/show.html.twig', compact('project'));
+    }
+}
